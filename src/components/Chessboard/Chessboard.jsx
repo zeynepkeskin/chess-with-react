3d432c890a44d60fa8f168a26c040a69e3bb4f8e
Zeynep Keskin <zeynepkeskin10@gmail.com>
2024-06-27 15:05:17 -0700

    added board and pieces

--- src/components/Chessboard/Chessboard.jsx
+++ src/components/Chessboard/Chessboard.jsx
@@ -29,6 +29,37 @@ for (let i = 0; i < 8; i++) {
     pieces.push({ image: "assets/images/pl.png", x: i, y: 1 });
 }
 
+let activePiece = null;
+
+function grabPiece(e) {
+    const element = e.target;
+    if(element.classList.contains("chess-piece")) {
+        const x = e.clientX -50;
+        const y = e.clientY -50;
+        element.style.position = "absolute";
+        element.style.left = `${x}px`;
+        element.style.top = `${y}px`;
+
+        activePiece = element;
+    }
+}
+
+function movePiece(e) {
+    if(activePiece) {
+        const x = e.clientX -50;
+        const y = e.clientY -50;
+        activePiece.style.position = "absolute";
+        activePiece.style.left = `${x}px`;
+        activePiece.style.top = `${y}px`;
+    }
+}
+
+function dropPiece(e) {
+    if(activePiece) {
+        activePiece = null;
+    }
+}
+
 export default function Chessboard() {
     let board = [];
 
@@ -47,5 +78,14 @@ export default function Chessboard() {
         }
     }
 
-    return <div id="chessboard">{board}</div>;
+    return (
+    <div 
+        onMouseMove={(e) => movePiece(e)} 
+        onMouseDown={(e) => grabPiece(e)} 
+        onMouseUp={(e) => dropPiece(e)}
+        id="chessboard"
+    >
+        {board}
+    </div>
+    );
 }
--- src/components/Tile/Tile.jsx
+++ src/components/Tile/Tile.jsx
@@ -2,8 +2,16 @@ import './Tile.css';
 
 export default function Tile({ number, image }) {
     if (number % 2 === 0) {
-        return <div className="tile black-tile"><img src={image}/></div>
+        return (
+        <div className="tile black-tile">
+            {image && <div style={{backgroundImage: `url(${image})`}} className='chess-piece'></div>}
+        </div>
+        );
     } else {
-        return <div className="tile white-tile"><img src={image}/></div>
+        return (
+        <div className="tile white-tile">
+            {image && <div style={{backgroundImage: `url(${image})`}} className='chess-piece'></div>}
+        </div>
+        );
     }
 }
